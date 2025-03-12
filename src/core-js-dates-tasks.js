388d0595fa2894c534f766dc03d9300f0310e10a
maiano <maiano@yandex.ru>
2025-03-12 21:20:08 +0100

    feat: 2 test pass

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -31,8 +31,13 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  return [hours, minutes, seconds]
+    .map((s) => s.toString().padStart(2, 0))
+    .join(':');
 }
 
 /**
