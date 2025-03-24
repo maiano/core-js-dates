6eb15f0462b9d70a0b1a90d1315c4d0deaa362ad
maiano <maiano@yandex.ru>
2025-03-24 23:28:34 +0100

    feat: 9 test pass

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -178,8 +178,18 @@ function formatDate(date) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const days = new Date(year, month, 0).getDate();
+
+  let count = 0;
+
+  for (let i = 1; i <= days; i += 1) {
+    const day = new Date(year, month - 1, i).getDay();
+    if (day === 0 || day === 6) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 /**
