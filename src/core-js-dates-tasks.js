928e4904252441a147a407fb9be83316e7385adf
maiano <maiano@yandex.ru>
2025-03-25 00:45:52 +0100

    feat: 14 test pass

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -265,8 +265,35 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '15-01-2024' }, 1, 3 => ['01-01-2024', '05-01-2024', '09-01-2024', '13-01-2024']
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
-function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
-  throw new Error('Not implemented');
+function getWorkSchedule(period, countWorkDays, countOffDays) {
+  const start = new Date(period.start.split('-').reverse().join('-'));
+  const end = new Date(period.end.split('-').reverse().join('-'));
+
+  const format = (date) => {
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
+  };
+
+  const currentDate = new Date(start);
+  const schedule = [];
+
+  while (currentDate <= end) {
+    for (let i = 0; i < countWorkDays; i += 1) {
+      if (currentDate <= end) {
+        schedule.push(format(currentDate));
+        currentDate.setDate(currentDate.getDate() + 1);
+      }
+    }
+
+    for (let j = 0; j < countOffDays; j += 1) {
+      if (currentDate <= end) {
+        currentDate.setDate(currentDate.getDate() + 1);
+      }
+    }
+  }
+  return schedule;
 }
 
 /**
@@ -281,8 +308,9 @@ function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 }
 
 module.exports = {
